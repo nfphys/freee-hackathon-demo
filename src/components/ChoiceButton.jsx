29e783c1411f2864bfd7ee2422fa13f77ae8ab8f
nfphys <nfphys@gmail.com>
2022-09-13 18:11:28 +0900

    コードの見た目を修正

--- src/components/ChoiceButton.jsx
+++ src/components/ChoiceButton.jsx
@@ -1,8 +1,8 @@
 export const ChoiceButton = (props) => {
 	const { value, onClick, disabled } = props
 	return (
-			<button className="choiceButton" onClick={onClick} disabled={disabled}>
+		<button className="choiceButton" onClick={onClick} disabled={disabled}>
 			{value}
-			</button>
+		</button>
 	);
 };
